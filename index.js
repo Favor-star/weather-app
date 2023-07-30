713657df9002e589f678adf5c50e05653d118ad8
Favor-star <favoureliab@gmail.com>
2023-07-30 10:42:41 +0200

    Final product

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-let city = "kigali";
-let url = `https://api.weatherapi.com/v1/forecast.json?key=9ed41df52aa54ba8b4b200722232807&days=5&q=${city}`;
-console.log(url);
+let city= 'Kigali';
+//create  search button functionalities
+let searchBar = document.getElementById('searchCity')
+let searchBtn = document.getElementById('searchIcon');
+searchBtn.addEventListener('click', getCity);
+searchBar.addEventListener('keyup', (event) =>{
+    if(event.key === 'Enter'){
+        getCity();
+    }
+})
+function getCity (){
+    if(searchBar.value == null || searchBar.value.trim() == "") {
+        city= 'Kigali';
+
+    }
+    else if (searchBar.value){
+        city = searchBar.value.trim();
+        
+    } 
+    getApi();
+}
+
 function getApi() {
+    let url = `https://api.weatherapi.com/v1/forecast.json?key=9ed41df52aa54ba8b4b200722232807&days=5&q=${city}`;
     fetch(url, {mode: "cors"})
     .then((response) => {
         return response.json();
     })
     .then((response) =>{
         console.log(response)
-        document.getElementById('feelsLike').innerHTML += `<span>${response.current.feelslike_c} <sup>0</sup>C</span>`
+        document.getElementById('feelsLike').innerHTML = `<span>${response.current.feelslike_c} <sup>0</sup>C</span>`
         document.getElementById('country').innerHTML = `${response.location.name} , ${response.location.country}`;
-        document.getElementById('latitude').innerHTML += `<span>${response.location.lat}</span>`;
-        document.getElementById('longitude').innerHTML += `<span>${response.location.lon}</span>`;
+        document.getElementById('latitude').innerHTML = `<span>${response.location.lat}</span>`;
+        document.getElementById('longitude').innerHTML = `<span>${response.location.lon}</span>`;
         document.getElementById('date').innerHTML = `<span>${date}</span>`;
-        document.getElementById('lastUpdated').innerHTML += `<span>${response.current.last_updated}`;
+        document.getElementById('lastUpdated').innerHTML = `<span>${response.current.last_updated}`;
         let condDiv = document.getElementById('conditions');
         condDiv.innerHTML = `<span>${response.current.condition.text} </span> <img src="${response.current.condition.icon}">`;
-        document.getElementById('pascal').innerHTML += `<span>${response.current.pressure_mb} mBar </span>`;
-        document.getElementById('humidity').innerHTML += `<span>${response.current.humidity}</span>`;
-        document.getElementById('wind').innerHTML += `<span class="wind"><span>${response.current.wind_mph}</span><span>${response.current.wind_degree} <sup>0</sup></span><span>${response.current.wind_dir}</span></span>`;
+        document.getElementById('pascal').innerHTML = `<span>${response.current.pressure_mb} mBar </span>`;
+        document.getElementById('humidity').innerHTML = `<span>${response.current.humidity}</span>`;
+        document.getElementById('wind').innerHTML = `<span class="wind"><span>${response.current.wind_mph}</span><span>${response.current.wind_degree} <sup>0</sup></span><span>${response.current.wind_dir}</span></span>`;
 
         let nextDates = document.querySelectorAll('.nextDates');
         let index =0;
@@ -32,9 +52,16 @@ function getApi() {
             elem.innerHTML = `<span><img src='${response.forecast.forecastday[newIndex].day.condition.icon}'></span>
             <span style="color:#CECE5A;">${response.forecast.forecastday[newIndex].day.condition.text}</span>
             <span>Max Temp: ${response.forecast.forecastday[newIndex].day.maxtemp_c} <sup>0</sup>C</span>
-            <span>Min Temp: ${response.forecast.forecastday[newIndex].day.mintemp_c} <sup>0</sup>C</span>`;
+            <span>Min Temp: ${response.forecast.forecastday[newIndex].day.mintemp_c} <sup>0</sup>C</span>
+            `;
             newIndex++;
         });
+        
+
+
+    })
+    .catch(error =>{
+        alert('Error message:', error);
     })
 }
 window.onload = getApi();
@@ -57,5 +84,5 @@ const time = new Date();
             'December'
           ];
           
-        const date = `${days[day]}, ${time.getDate()} ${months[time.getMonth()]} ${time.getFullYear()}`;
+        const date = `${days[day]}, ${time.getDate()}th ${months[time.getMonth()]} ${time.getFullYear()}`;
     
